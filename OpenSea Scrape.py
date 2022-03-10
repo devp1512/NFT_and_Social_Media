9faca9b70b3f9dedca4ed512c265f114c9c4198f
devp1512 <86944611+devp1512@users.noreply.github.com>
2022-03-10 13:41:48 +0800

    Update OpenSea Scrape.py

--- OpenSea Scrape.py
+++ OpenSea Scrape.py
@@ -22,13 +22,9 @@ project_raw = driver.find_element(By.XPATH, project_heading)
 
 while True:
     try:
-        print("2")
         project_heading = f'//*[@id="main"]/div/div[2]/div/div[2]/div[9]/a'
-        print("2")
         project_raw = driver.find_element(By.XPATH, project_heading)
-        print("2")
         print(project_raw.text)
-        print("2")
         index += 1
     except:
         break
